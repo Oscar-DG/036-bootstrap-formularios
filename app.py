8735d798da38b0b37c1331921637308c57d51168
Oscar David García López <ogarcial9@miumg.edu.gt>
2026-09-02 23:56:19 -0600

    Plantilla base Bootstrap y pagina de inicio

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def inicio():
-    return "Proyecto Semana 8 funcionando"
+    return render_template("inicio.html")
 
 
 if __name__ == "__main__":
